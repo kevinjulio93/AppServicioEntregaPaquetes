decc9088c279d42a208fa1614bfe729c0bed0d3a
kevin julio <kevinjulio93@outlook.com>
2015-03-09 18:52:32 -0500

    primera actualizacion 6:52 pm

---
+++ AppServicioEntregaPaquetes/Modelo/Package.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppServicioEntregaPaquetes.Modelo
+{
+    public class Package
+    {
+
+        #region "Atributos"
+        public string codigo;
+        public string nombre;
+        public string direccion;
+        public string ciudad;
+        public string departamento;
+        public string cp_remitente;
+        public string cp_destinatrario;
+        public int peso;
+        public int costo_gramos;
+        public int valor_envio;
+        #endregion
+        
+        #region "Propiedades"
+        public String Codigo { get; set; }
+        public String Nombre { get; set; }
+        public String Direccion { get; set; }
+        public String Ciudad { get; set; }
+        public String Departamento { get; set; }
+        public String Cp_remitente { get; set; }
+        public String Cp_destinatario { get; set; }
+        public int Peso { get; set; }
+        public int Costo_gramos { get; set; }
+        public int Valor_envio { get; set; }
+        #endregion   
+
+        #region "Contructores"
+        public Package()
+        {
+
+            this.codigo = "12345";
+            this.nombre = "kevin julio";
+            this.direccion = "universidad tecnologica de bolivar, km q via turbaco";
+            this.ciudad= "cartagena";
+            this.departamento= "bolivar";
+            this.cp_remitente = "4-72";
+            this.cp_destinatrario = "7-42";
+            this.peso = 10;
+            this.costo_gramos = 100;
+            this.valor_envio = 0;
+                
+
+        }
+
+        public Package(String codigo, String nombre, String direccion, String ciudad, String departamento, String cp_remitente, String cp_destinatario, int peso, int costo_gramos, int valor_envio)
+        {
+
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.ciudad = ciudad;
+            this.departamento = departamento;
+            this.cp_remitente = cp_remitente;
+            this.cp_destinatrario = cp_destinatario;
+            this.peso = peso;
+            this.costo_gramos = costo_gramos;
+            this.valor_envio = valor_envio;
+
+
+        }
+        #endregion
+
+        #region "metodos"
+        public int calculatecost()
+        {
+
+            valor_envio = (peso * 1000) * costo_gramos;
+            return valor_envio;
+        }
+        #endregion
+
+        #region "ToString"
+
+        public override string ToString()
+        {
+            return "Codigo : " + codigo +
+                    "\nNombre : " + nombre +
+                    "\nDireccion : " + direccion +
+                    "\nCiudad : " + ciudad +
+                    "\nDepartamento : " + departamento +
+                    "\nCodigo postal remitente : " + cp_remitente +
+                    "\nCodigo postal destinatario : " + cp_destinatrario +
+                    "\nPeso Paquete : " + peso +
+                    "\nCosto por gramo : " + costo_gramos +
+                    "\nValor total envio : " + calculatecost() + "se llevo e kilogramos a gramos para hallar el valor en gramos";
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        #endregion
+
+
+    }
+
+}
